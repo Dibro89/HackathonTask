4dff37d4377aa2c7a82adab1ecdc6a116fd72b49
Dima Melikhov <dima.mel8@gmail.com>
2017-02-19 21:54:08 +0200

    Cleanup

--- app/src/main/kotlin/dika/hackathontask/MainActivity.kt
+++ app/src/main/kotlin/dika/hackathontask/MainActivity.kt
@@ -4,145 +4,141 @@ import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
+import android.view.ViewManager
 import android.widget.DatePicker
 import com.github.kittinunf.fuel.android.extension.responseJson
 import com.github.kittinunf.fuel.httpGet
-import com.jjoe64.graphview.GraphView
-import com.jjoe64.graphview.LegendRenderer
-import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter
-import com.jjoe64.graphview.series.DataPoint
-import com.jjoe64.graphview.series.LineGraphSeries
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import org.jetbrains.anko.*
+import org.jetbrains.anko.custom.ankoView
 import java.text.SimpleDateFormat
 import java.util.*
 
+const val privatbankAPI = "https://api.privatbank.ua/p24api/exchange_rates"
+const val dayInMillis: Long = 1000 * 60 * 60 * 24
+
 class MainActivity : AppCompatActivity() {
+    private val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+    private val calendar = Calendar.getInstance()
+
+    private lateinit var pckFrom: DatePicker
+    private lateinit var pckTo: DatePicker
+    private lateinit var chart: BarChart
+
+    private var from: Long = -1
+    private var to: Long = -1
+    private var days: Long = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MainActivityUI().setContentView(this)
-    }
-}
 
-@Suppress("DEPRECATION")
-class MainActivityUI : AnkoComponent<MainActivity> {
-    override fun createView(ui: AnkoContext<MainActivity>) =
-            with(ui) {
+        verticalLayout {
+            gravity = Gravity.CENTER
+
+            linearLayout {
+                gravity = Gravity.CENTER
+
                 verticalLayout {
-                    gravity = Gravity.CENTER
+                    lparams { margin = 10 }
+                    textView("From:") { gravity = Gravity.CENTER; textSize = 20f }
+                    pckFrom = datePicker { calendarViewShown = false }
+                }
 
-                    val graph = GraphView(ui.ctx)
+                verticalLayout {
+                    lparams { margin = 10 }
+                    textView("To:") { gravity = Gravity.CENTER; textSize = 20f }
+                    pckTo = datePicker { calendarViewShown = false }
+                }
+            }
 
-                    var fromPicker: DatePicker? = null
-                    var toPicker: DatePicker? = null
+            button("Show") {
+                lparams { margin = dip(10); width = dip(150) }
+                onClick {
+                    chart.data = null
 
-                    linearLayout {
-                        gravity = Gravity.CENTER
+                    from = pckFrom.millis
+                    to = pckTo.millis
 
-                        verticalLayout {
-                            textView("From:") { gravity = Gravity.CENTER; textSize = 20f }
-                            fromPicker = datePicker { calendarViewShown = false }
-                        }.lparams { margin = 10 }
+                    if (to < from) to = from
 
-                        verticalLayout {
-                            textView("To:") { gravity = Gravity.CENTER; textSize = 20f }
-                            toPicker = datePicker { calendarViewShown = false }
-                        }.lparams { margin = 10 }
-                    }
+                    days = (to - from) / dayInMillis + 1
 
-                    button("Show") {
-                        onClick {
-                            graph.removeAllSeries()
+                    doAsync {
+                        val pb = mutableListOf<BarEntry>()
+                        val nb = mutableListOf<BarEntry>()
 
-                            val from = Calendar.getInstance().apply {
-                                fromPicker!!.let { set(it.year, it.month, it.dayOfMonth) }
-                            }.time.time
+                        for (i in 0 until days) {
+                            val date = Date(from + i * dayInMillis)
 
-                            val to = Calendar.getInstance().apply {
-                                toPicker!!.let { set(it.year, it.month, it.dayOfMonth) }
-                            }.time.time
+                            val (request, response, result) = privatbankAPI
+                                    .httpGet(listOf("json" to "", "date" to dateFormat.format(date)))
+                                    .responseJson()
 
-                            val step: Long
-
-                            val delta = from - to
-                            if (delta > 1000 * 60 * 60 * 24 * cols) {
-                                step = delta / cols
-                            } else step = 1000 * 60 * 60 * 24
-
-                            val s = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
-
-                            val pb = arrayOfNulls<DataPoint>(cols)
-                            val nb = arrayOfNulls<DataPoint>(cols)
-
-                            ui.doAsync {
-                                for (i in 0..cols - 1) {
-                                    val d = Date(from + step * i)
-
-                                    val (request, response, result) = p24.httpGet(
-                                            listOf(
-                                                    "json" to "",
-                                                    "date" to s.format(d)
-                                            )
-                                    ).responseJson()
-
-                                    println(result.get().content)
-
-                                    val arr = result.get().obj().getJSONArray("exchangeRate")
-                                    for (j in 0..arr.length() - 1) {
-                                        val obj = arr.getJSONObject(j)
-                                        if (obj.getString("currency") == "USD") {
-                                            pb[i] = DataPoint(d, obj.getDouble("saleRate"))
-                                            nb[i] = DataPoint(d, obj.getDouble("saleRateNB"))
-                                        }
-                                    }
-                                }
-
-                                activityUiThread {
-                                    graph.addSeries(LineGraphSeries(pb).apply {
-                                        title = "PrivatBank"
-                                        color = Color.GREEN
-                                    })
-
-                                    graph.addSeries(LineGraphSeries(nb).apply {
-                                        title = "National Bank of Ukraine"
-                                        color = Color.BLUE
-                                    })
-
-                                    graph.gridLabelRenderer.apply {
-                                        labelFormatter = DateAsXAxisLabelFormatter(graph.context)
-                                        numVerticalLabels = 5
-                                        numHorizontalLabels = cols
-//                                        isHumanRounding = false
-                                    }
-
-                                    graph.viewport.apply {
-                                        setMinX(pb.first()!!.x)
-                                        setMaxX(pb.last()!!.x)
-                                        isXAxisBoundsManual = true
-                                    }
-
-                                    graph.legendRenderer.apply {
-                                        isVisible = true
-                                        align = LegendRenderer.LegendAlign.TOP
-                                        backgroundColor = Color.GRAY
-                                    }
+                            val arr = result.get().obj().getJSONArray("exchangeRate")
+                            for (j in 0 until arr.length()) {
+                                val obj = arr.getJSONObject(j)
+                                if (obj.getString("currency") == "USD") {
+                                    pb.add(BarEntry(i.toFloat(), obj.getDouble("saleRate").toFloat()))
+                                    nb.add(BarEntry(i.toFloat(), obj.getDouble("saleRateNB").toFloat()))
                                 }
                             }
                         }
-                    }.lparams { topMargin = -20; width = 250 }
 
-                    addView(graph)
+                        uiThread {
+                            chart.apply {
+                                data = BarData(mutableListOf<IBarDataSet>(
+                                        BarDataSet(pb, "PrivatBank")
+                                                .apply { color = Color.GREEN },
+                                        BarDataSet(nb, "National Bank of Ukraine")
+                                                .apply { color = Color.BLUE }
+                                ))
+
+                                val groupSpace = 0.16f
+                                val barSpace = 0.06f
+                                val barWidth = 0.36f
+
+                                barData.barWidth = barWidth
+                                groupBars(0f, groupSpace, barSpace)
+
+                                xAxis.axisMaximum = days.toFloat()
+
+                                chart.invalidate()
+                            }
+                        }
+                    }
                 }
             }
-}
 
-const val p24 = "https://api.privatbank.ua/p24api/exchange_rates"
-const val cols = 3
+            chart = barChart {
+                lparams { margin = dip(30); width = matchParent; height = matchParent }
 
-/*
-    public class MainActivity extends AppCompatActivity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+                description.isEnabled = false
+
+                xAxis.apply {
+                    axisMinimum = 0f
+                    setCenterAxisLabels(true)
+                    setValueFormatter { value, axisBase ->
+                        dateFormat.format(Date(from + value.toLong() * dayInMillis))
+                    }
+                }
+
+                axisLeft.apply {
+                    axisMinimum = 25f
+                    axisMaximum = 28f
+                    setDrawGridLines(false)
+                }
+
+                axisRight.isEnabled = false
+            }
         }
     }
-*/
+
+    val DatePicker.millis: Long get() = calendar.apply { set(year, month, dayOfMonth) }.time.time
+}
+
+inline fun ViewManager.barChart(theme: Int = 0, init: BarChart.() -> Unit)
+        = ankoView(::BarChart, theme, init)
